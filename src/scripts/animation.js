d22d5c02c583936b6f3bda25576e8ec88d430173
Andrew Wragg <andrewwragg1@gmail.com>
2023-12-07 18:05:22 +0000

    Added animation script file and css file and added some scroll animation class names to index.html.
    Methodology is to add animation class to element in html, this class will set the initial changed state of the element.  Then the intersection observer will detect when the element is on screen and apply the active class to it, which will trigger the element to animate to its normal state.

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -1,3 +1,67 @@
 export function init() {
 
+    const inOptions = {
+        root: null,
+        rootMargin: '0px 300% 0px 300%',
+        threshold: 0.25
+    };
+
+    const outOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.25
+    };
+
+    let theVideo = document.getElementById('theVideo');
+
+    let elementList = document.querySelectorAll('#thenecklace, .fade-in, .from-bottom, .from-left, .from-right, .blur');
+    console.log(elementList);   
+    
+    /*let fadeOutList = document.querySelectorAll('.fade-out');
+    let slideInList = document.querySelectorAll('.slide-in');
+    let slideOutList = document.querySelectorAll('.slide-out');
+    let scaleInList = document.querySelectorAll('.scale-in');
+    let scaleOutList = document.querySelectorAll('.scale-out');
+    let rotateInList = document.querySelectorAll('.rotate-in');
+    let rotateOutList = document.querySelectorAll('.rotate-out');
+    let skewInList = document.querySelectorAll('.skew-in');
+    let skewOutList = document.querySelectorAll('.skew-out');
+    let blurInList = document.querySelectorAll('.blur-in');
+    let blurOutList = document.querySelectorAll('.blur-out');
+    */
+
+
+    const callbacks = enteries => {
+        enteries.forEach(entry => {
+            console.log(entry.target);
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+
+                if (entry.target.id == 'thenecklace') {
+                    playVid(theVideo);
+                }
+            } 
+        });
+    }
+
+    const videoObserver = new IntersectionObserver(callbacks, inOptions);
+    videoObserver.oserve = theVideo;
+    const observer = new IntersectionObserver(callbacks, inOptions);
+
+
+
+    elementList.forEach(element => {
+        observer.observe(element);
+    });
+
+
+}
+
+
+function playVid(vidElement) {
+    vidElement.play();
+}
+
+function pauseVid(vidElement) {
+    vidElement.pause();
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import '../css/style.css'
 import '../css/navigation.css'
+import '../css/animation.css'
 import { init as initNav } from './navigation.js'
 import { init as initAnim } from './animation.js'
 const { BASE_URL } = import.meta.env
